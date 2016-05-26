f620b7cd74acb9845b89daa18dbdb43c77d74b7b
bgacek <bartlomiejgacek1@gmail.com>
2016-05-26 03:32:09 +0200

    TEST 3 albo ide spac

---
+++ AirHokey/Bot.h
@@ -0,0 +1,28 @@
+
+#include<SFML/Graphics.hpp>
+#include "Plansza.h"
+class Bot
+{
+public:
+	Bot(std::string polowa , Plansza * plansza , Krazek * Pilka);
+	~Bot();
+	void move();
+	void rysuj(sf::RenderWindow * window);
+	void moveTo(float x , float y);
+	sf::Vector2f getKierunek();
+	sf::CircleShape getShape();
+	sf::CircleShape krazek;
+	
+private:
+	std::string polowa;
+	Krazek * pilka;
+	void move_right_side();
+	void move_left_side();
+	float speed_do_lini = 0.4f;
+	float speed_do_krazka = 0.4f;// 0.2f;<-poziom low xd / 0.4f <-poziom kozak xd
+	int linia_przy_bramce = 100;//powinienem ja wyznaczac !
+	//int wymiary[2];//wymiary planszy /0:szerokosc /1:wysokosc
+	//sf::CircleShape krazek;
+	Plansza *plansza;
+	sf::Vector2f kierunek;
+};
